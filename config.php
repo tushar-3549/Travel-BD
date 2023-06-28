1bf514c32a83200dd6511bfc8f388b3561240d39
Md. Tushar Ahmed <99356217+tushar-3549@users.noreply.github.com>
2023-06-28 11:44:29 +0600

    config.php

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+// connecting database
+$servername = "localhost";
+$username = "root";
+$password = "";
+// create connection
+$connection = mysqli_connect($servername, $username, $password, "web_project");
+// if($connection){
+//     echo "\nCongratulations! Sign In Successful";
+// }
+// else{
+//     die("Sorry! Failed to connection : ". mysqli_connect_error());
+// }
+// create DB
+// $mysql = "CREATE DATABASE users";
+// $result = mysqli_query($connection, $mysql);
+// if the same name db then result=false, otherwise true
+// echo "The result is : ";
+// echo var_dump($result);
+// echo "<br>";
+?>
